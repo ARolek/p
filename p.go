1bc76936aa157179538f1cd815ce99ca3af80ff0
Alexander Rolek <a.rolek@gmail.com>
2016-11-23 15:30:59 -0800

    initial commit

---
+++ p.go
@@ -0,0 +1,36 @@
+//	pacakge p takes in values and returns a pointer to the value
+package p
+
+import "time"
+
+func Bool(b bool) *bool {
+	return &b
+}
+
+func String(str string) *string {
+	return &str
+}
+
+func Int(i int) *int {
+	return &i
+}
+
+func Int32(i int32) *int32 {
+	return &i
+}
+
+func Int64(i int64) *int64 {
+	return &i
+}
+
+func Float32(f float32) *float32 {
+	return &f
+}
+
+func Float64(f float64) *float64 {
+	return &f
+}
+
+func Time(t time.Time) *time.Time {
+	return &t
+}
